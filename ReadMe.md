d14d7fa7094b1a4a5f30e097aa79200d852548ab
Shreyans Khabia <shreyans.khabia@gmail.com>
2021-09-05 17:22:56 +0530

    commiting

---
+++ ReadMe.md
@@ -0,0 +1,25 @@
+# Spotify Autologin
+
+This script opens a VPN, logs into spotify with your credentials and then closes the VPN.
+
+this is very useful if you have set your spotify country different from where you live (as I have done because many artists are only available on Spotify in certain regions), as you can only use a Spotify account in a different country or region for up to 14 days, before asking to login again form your accounts spotify country.
+
+## required library/software
+<p>software:  
+
+        VPN: Psiphon3 (you can modify the code to open VPn of your choice)
+</p>
+
+<p>libraries:
+
+        selenium  
+    	time  
+    	datetime  
+    	os
+</p>
+
+
+
+#### note:
+if an error occurs, update the class name of the Login Button in the code using inspect element in your browser(see comment in the code).  
+PS: this is a rare event.
---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import time
+from selenium import webdriver
+from datetime import date
+
+
+psiphonpath = r'C:\Users\user\Downloads\psiphon3.exe'           # path to psiphon3.exe
+Webdriverpath = r'C:\Users\user\Downloads\chromedriver.exe'     # path to selenium webdriver (i am using chrome webdriver)
+
+emailID = r""   # enter emailID or username
+password = r""  # enter password
+
+while 1:
+    print("opening Psiphon3")
+    os.startfile(psiphonpath)
+    time.sleep(20)              # giving time for psiphon to initialize
+    print("Psiphon3 must be open and ready")
+
+    driver = webdriver.Chrome(Webdriverpath)
+    driver.get("https://open.spotify.com/")
+    time.sleep(3)
+    LoginButt = driver.find_element_by_class_name("PzcmS_Z8j0D6n3ZEmv20.Hy_P64B8lNKgp3N7Qz4Z")  # if an error occurs ,update this as the class name may have changed
+    LoginButt.click()
+
+    time.sleep(5)
+    emailbox = driver.find_element_by_id("login-username")
+    emailbox.send_keys(emailID)
+    print("email filled")
+
+    passbox = driver.find_element_by_id("login-password")
+    passbox.send_keys(password)
+    print("password filled")
+
+    LoginButt = driver.find_element_by_id("login-button")
+    time.sleep(4)
+    LoginButt.click()
+    # LoginButt.submit()
+
+    today = date.today()
+    d2 = today.strftime("%B %d, %Y")
+    print(f"login done {d2}")
+
+    time.sleep(20)      # this allows login to not be interrupted due to sudden closing of psiphon
+    print("closing psiphon ")
+    os.system("TASKKILL /IM psiphon3.exe")
+    # os.system("TASKKILL /IM Google Chrome.exe")
+    time.sleep(604800)  # you can change re-logging time to a time that suits you, i have set it to 7 days
+
